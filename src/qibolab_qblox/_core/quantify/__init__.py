2fd7fb78bfc3ae16f0c4bc72f1b4dd3705623bd7
Alessandro Candido <candido.ale@gmail.com>
2025-11-26 11:06:17 +0400

    feat: Initialize the quantify based driver

---
+++ src/qibolab_qblox/_core/quantify/__init__.py
@@ -0,0 +1,25 @@
+from qibolab import Config, ExecutionParameters, ParallelSweepers, PulseSequence, Result
+from qibolab._core.instruments.abstract import Controller
+
+
+class QuantifyCoordinator(Controller):
+    bounds: str = "quantify/bounds"
+
+    @property
+    def sampling_rate(self) -> int:
+        return 1
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def play(
+        self,
+        configs: dict[str, Config],
+        sequences: list[PulseSequence],
+        options: ExecutionParameters,
+        sweepers: list[ParallelSweepers],
+    ) -> dict[int, Result]:
+        return {}
